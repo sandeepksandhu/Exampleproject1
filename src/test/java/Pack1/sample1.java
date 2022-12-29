078b68e96fe427ac748a54c3eb80508a2d989b50
sandeep <sandeep.jotbir@gmail.com>
2022-12-28 22:07:15 -0500

    commit again

--- src/test/java/Pack1/sample1.java
+++ src/test/java/Pack1/sample1.java
@@ -4,5 +4,6 @@ public class sample1 {
     public static void main(String[] args)
     {
         System.out.println("Hello how are you");
+        System.out.println("enter the name and trying");
     }
 }
